0faaa708b6fe3a93f179b861e6e2ccd8e8f9924f
DeliniS <114213808+DeliniS@users.noreply.github.com>
2023-12-08 01:28:31 +0530

    initialized springboot using maven

---
+++ grasscutter/src/main/java/com/example/grasscutter/GrasscutterApplication.java
@@ -0,0 +1,13 @@
+package com.example.grasscutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrasscutterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrasscutterApplication.class, args);
+	}
+
+}
