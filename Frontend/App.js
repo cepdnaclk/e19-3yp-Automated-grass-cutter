5cc146ff8dc992a47cda90c5ae3cfe255960c95a
Thevambikai <110016117+Thevambikai@users.noreply.github.com>
2023-12-31 23:48:29 +0530

    Add signup and login pages to app.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,41 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView, Image } from 'react-native';
-//View -> UIView
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Welcome from './screens/Welcome';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  console.log("App executed");
-  
-
   return (
-    <SafeAreaView style={styles.container}>
-      {/* Logo Image */}
-      <Image
-        source={require("./logo.png")}  
-        style={styles.logo}
-      />
-      <Text style={styles.text}>LAWNMATE</Text>
-
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#DAFADA',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 300,  // Set the new width of your logo
-    height: 300, // Set the new height of your logo
-    marginBottom: -40, // Adjust as needed
-  },appName: {
-    fontSize: 40,  // Set the font size
-    fontWeight: 'bold',  // Set the font weight (e.g., 'bold', 'normal', etc.)
-    fontStyle: 'italic', // Set the font style (e.g., 'normal', 'italic')
-    color: 'black', // Set the text color
-  },text: {
-    fontSize: 50,
-  },
-});
