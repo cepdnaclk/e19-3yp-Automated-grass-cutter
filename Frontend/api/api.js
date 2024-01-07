686318f47c0209a545da094b5950bfa7ee6c2b3b
Thevambikai <e19494@eng.pdn.ac.lk>
2024-01-07 17:51:22 +0530

    Change Login page

--- Frontend/api/api.js
+++ Frontend/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/users/signup';
+const API_URL_Login = 'http://localhost:8080/users/signin';
 
 export const signupUser = async (email, password) => {
   try {
@@ -11,6 +12,18 @@ export const signupUser = async (email, password) => {
     return response.data;
   } catch (error) {
     console.error('Error signing up:', error);
+    throw error;
+  }
+};
+export const loginUser = async (email, password) => {
+  try {
+    const response = await axios.post(API_URL_Login, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error logging in:', error);
     throw error;
   }
 };
